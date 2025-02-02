fc6d4dd6e22b41aaf79bcee8e08e556797dc4e19
Chris Guilliams <chrisg07@vt.edu>
2025-02-02 11:39:18 -0600

    removed dead code

--- main.lua
+++ main.lua
@@ -54,18 +54,6 @@ function love.update(dt)
     contextMenu:update(dt, world.character, world.grid, state)
 end
 
-local function drawProgressBar(x, y, width, height, progress)
-    love.graphics.setColor(0.3, 0.3, 0.3) -- dark gray background
-    love.graphics.rectangle("fill", x, y, width, height)
-    love.graphics.setColor(0.1, 0.8, 0.1) -- green fill
-    love.graphics.rectangle("fill", x, y, width * progress, height)
-    love.graphics.setColor(1, 1, 1)       -- white border
-    love.graphics.rectangle("line", x, y, width, height)
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.setColor(1, 1, 1)
-    -- love.graphics.print(string.format("EXP: %d / %d", state.experience, 100), x, y + height + 5)
-end
-
 function love.draw()
     background:draw()
     world:draw()
